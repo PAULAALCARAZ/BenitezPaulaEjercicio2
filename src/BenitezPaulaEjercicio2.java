9e00894867416ec2d08a8201f20eae9ba660126d
paula <paulayaqueline.benitez@gracia.lasalle.cat>
2024-11-17 18:04:36 +0100

    V5 agregue un control de errores al solicitar tamaño, para avitar letras.

--- src/BenitezPaulaEjercicio2.java
+++ src/BenitezPaulaEjercicio2.java
@@ -4,17 +4,35 @@ public class BenitezPaulaEjercicio2 {
     public static void main (String []args){
         Scanner sc= new Scanner(System.in);
 
-        int tamanho;
+        //declaramos variable
+        int tamanho=0;
+        boolean inputValido = false;
 
         System.out.println("Hola! vamos a crear tu tablero personalizado"+ "\n Introduce el caracter que quieres usar como patron.");
         char patron = sc.next().charAt(0); //usamos char y charAt por que queremos usar solo el primer caracter.
 
         //pedimos al usuario el tamaño de la casilla
 
-        do {
-            System.out.println("ahora introduce el tamaño que quieres para la casilla (El valor debe estar entre 1 y 15): ");
-            tamanho= sc.nextInt();
-        }while (tamanho < 1 || tamanho > 15); //determinamos rango
+        while (!inputValido) {
+            System.out.println("Introduce el tamaño que quieres para la casilla (El valor debe estar entre 1 y 15): ");
+
+            // Comprobamos si la siguiente entrada es un número entero usando "hasNextInt
+            if (sc.hasNextInt()) {
+                tamanho = sc.nextInt();
+
+                // Verificar que el tamaño esté dentro del rango permitido
+                if (tamanho >= 1 && tamanho <= 15) {
+                    inputValido = true; // Si el valor es válido, salimos del bucle
+                } else {
+                    System.out.println("El tamaño debe estar entre 1 y 15. Por favor, intenta nuevamente.");
+                }
+            } else {
+                // Si no es un número entero, mostramos un mensaje
+                System.out.println("Error: Por favor, ingresa un número valido.");
+                sc.next(); // Leemos nueva entrada
+            }
+        }
+
 
         int borde= 8; //porque un tablero de ajedrez debe tener 8x8
 
